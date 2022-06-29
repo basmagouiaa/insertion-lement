d3e728b2b67735d427bfb0405c907f2514cc5232
basmagouiaa <gouiaa.basmaa@gmail.com>
2022-06-29 12:39:42 +0100

    modicica

--- calcul.php
+++ calcul.php
@@ -1,7 +1,30 @@
 <html>
 <body>
 
-Votre tableau est : <br> <?php echo  $_POST["l"]; ?><br> 
+Votre tableau est : <br> <?php echo  $_POST["l"]; ?><br>
+
+<?php
+$t = explode(" ",$_POST["l"]);
+sort($t);
+
+$ma = max($t);
+
+echo "Votre tableau est : <br>". $t ."<br>";
+
+echo "Votre tableau trié est : <br>" ;
+
+
+$clength=count($t);
+echo " clength = ".$clength ."<br>";
+for($i=0;$i<$clength;$i++)
+  {
+  echo $t[$i]." ";
+  }
+
+echo "<br>";
+echo "Min est : ". $t[2] . "<br>";
+echo "Max est : ". $ma . "<br>";
+?>
 
 </body>
 </html>
