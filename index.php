455ba77c98bdd6edb95eaeaf87f44103d6560a00
basmagouiaa <gouiaa.basmaa@gmail.com>
2022-06-29 09:44:49 +0100

    page html

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Insertion</title>
+
+<script>
+
+var input= document.getElementById('c').value;
+</script>
+</head>
+<body>
+
+
+<p>Inserer element</p>
+
+<input type="text" id="c" >
+    
+<button type="button">+</button> <br>
+<p>tableau</p> <br>
+<button type="button">calculer</button> <br>
+
+
+</body>
+</html>
